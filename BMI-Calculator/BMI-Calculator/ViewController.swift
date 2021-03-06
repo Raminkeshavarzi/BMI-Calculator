52f7c06364dad4aa1cc1a13f4b0c31a29a18e680
Ramin Keshavarzi <you@example.com>
2021-03-06 20:55:08 +0100

    added components

--- BMI-Calculator/BMI-Calculator/ViewController.swift
+++ BMI-Calculator/BMI-Calculator/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//Components
+    
+    @IBOutlet weak var heightInput: UITextField!
+    @IBOutlet weak var weightInput: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = ""
     }
 
+//Function
+    @IBAction func calculateBmi(_ sender: Any) {
+        let height = Double(heightInput.text!)!
+        let weight = Double(weightInput.text!)!
+//        calculation
+        let bmi = Double(weight / (height * height))
+        let bodyMassIndex = Double(round(1000*bmi)/1000)
 
+        label.text = "Your BMI is \(bodyMassIndex)"
+    }
 }
 
